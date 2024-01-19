85f2bcc4443ca8fd044b746726d349805a51aa8c
VivianaGonzales <vivita2504@gmail.com>
2024-01-19 19:36:42 -0300

    axios

--- controllers/axiosController.js
+++ controllers/axiosController.js
@@ -1,9 +1,13 @@
 const axiosController = {
-
-    async axiosGet (req,res) {
-        const listadoDeTurnos = await axiosGet
+    async axiosGet(req, res) {
+        try {
+            const { data } = await axios.Get('https://proyectobackend-production-ad9c.up.railway.app/api/info')
+            return data
+        } catch (error) {
+            
+        }
     }
+
 }
 
 module.exports = axiosController
-
